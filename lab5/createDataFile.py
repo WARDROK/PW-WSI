638e7c089d5fa30bc38f8dec37c665a95ef33b89
lszydlik <01187295@pw.edu.pl>
2024-12-31 10:31:44 +0100

    Prepare data

---
+++ lab5/createDataFile.py
@@ -0,0 +1,14 @@
+from ucimlrepo import fetch_ucirepo
+import pandas as pd
+
+# fetch dataset
+wine_quality = fetch_ucirepo(id=186)
+
+# data (as pandas dataframes)
+X = wine_quality.data.features
+Y = wine_quality.data.targets
+# Y = Y.replace({'M': 1, 'B': 0})
+
+# Save to CSV file
+df = pd.concat([Y, X], axis=1)
+df.to_csv("data.csv", index=False)
